d194d3b61b9b4d97b82011a2b78752ffe4e3fbda
Ai686Leo <104388319+Ai686Leo@users.noreply.github.com>
2025-01-18 00:24:38 +0800

    Update action.sh

--- action.sh
+++ action.sh
@@ -6,14 +6,14 @@ SERVICE="${0%/*}/service.sh"
 # 检查 aria2c 进程是否存在并处理  
 if pgrep aria2c > /dev/null 2>&1; then  
   echo "Aria2c正在运行，正在杀死进程……"  
-  # 如果进程存在，使用 pkill 终止  
-  pkill aria2c 
+    # 如果进程存在，使用 pkill 终止  
+    pkill aria2c 
   sleep 0.7
   echo "已杀死进程"  
 else  
   echo "Aria2c未运行，正在启动……"  
-  # 如果进程不存在，调用 service.sh 启动服务  
-  sh "$SERVICE"  
+    # 如果进程不存在，调用 service.sh 启动服务  
+    sh "$SERVICE"  
   sleep 0.7
   echo "已成功启动"  
 fi  
